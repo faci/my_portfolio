a67bc09646dffbfffbe68918b0de20df4838ef85
Lisa Faci <lisa.faci@epitech.eu>
2025-06-27 20:14:02 -0500

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,34 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Header from '@/app/ui/header';
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Lisa Faci - Portfolio",
+  description: "Portfolio of Lisa Faci - freelance fullstack developer",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
+        {children}
+      </body>
+    </html>
+  );
+}
